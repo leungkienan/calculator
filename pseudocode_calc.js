2515ecd7600ba88293ef9ade76078596f173ff4c
Kienan Leung <leung.kienan@gmail.com>
2020-10-08 12:03:08 +1300

    added algorithms to js

--- pseudocode_calc.js
+++ pseudocode_calc.js
@@ -1,10 +1,11 @@
 let count = 0;
+let calcArr = [];
 
 startCalc();
 
 function startCalc() {
-    document.addEventListener("click", buttonPress);
-    console.log("js initialised");
+  document.addEventListener("click", buttonPress);
+  console.log("js initialised");
 }
 
 // when a button is clicked, call a function that checks what button is pressed and go through the methods
@@ -12,36 +13,42 @@ function buttonPress() {
   let val = event.target.value;
   if (!isNaN(val) || val == ".") {
     inputValue(val);
-  } else if(val == 'AC'){
-      clearAC()
-  } else if(val == 'CE'){
-      clearCE()
-  } else if(val == '='){
-      calcValue()
-  } else{
-      storeValue(val)
+  } else if (val == "AC") {
+    clearAC();
+  } else if (val == "CE") {
+    clearCE();
+  } else if (val == "=") {
+    calcValue();
+  } else {
+    storeValue(val);
   }
 }
 
 // if a number is pressed, store the value to "val" and display that number on the screen as a string
-function inputValue(key) {
-  console.log("The number "+ key+ " button works ");
+function inputValue(num) {
+  calcArr.push(num);
+  console.log(calcArr)
+  console.log("The number " + num + " button works ");
 }
 
 // if the ac button is pressed
 // reset the entries and temp data variables so they are empty and the totals to 0
 // display the value (0)
-function clearAC(){
-    
+function clearAC() {
+  calcArr = [];
+  console.log(calcArr)
 }
-
 // if ce is pressed
 // clears the previous entry from the screen
 // set temp to '' and display the value 0
-function clearCE(){
-    
-}
+function clearCE() {}
 
+function storeValue(symbol) {
+  if (symbol == "*") {
+    calcArr.push(symbol);
+    console.log(calcArr);
+  }
+}
 // multiply is pressed
 // push a value "temp" to the entries array
 // then push a *, so the array looks like entries = [temp, *]
@@ -50,6 +57,13 @@ function clearCE(){
 // divide symbol
 // same as above, but push '/' instead
 
+function calcValue(){
+    // turn the values in the array to nums
+    // do the correct operations depending on what buttons are pressed
+    // e.g. 6+7 should = 13
+    // challenge is for the calculator to do bedmas?
+        // e.g. takes the numbers before and after the "x" and multiply them 
+}
 // equals symbol
 // push temp to entries array
 // sets var nt to the the first number of the entries array
